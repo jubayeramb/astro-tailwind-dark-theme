8fd9edf355bce45a2401588109b35cc8128dc3d6
Jubayer Al Mamun <jubayeramb@gmail.com>
2023-11-09 23:45:07 +0600

    feat: add dark theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,12 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bgColor: "rgb(var(--color-bgColor) / <alpha-value>)",
+        contentColor: "rgb(var(--color-contentColor) / <alpha-value>)",
+      },
+    },
   },
   plugins: [],
 };
